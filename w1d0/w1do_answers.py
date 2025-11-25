56680b7a1d84603a213f47cda527c8ffb5b7de7e
Amruta Veer <amrutaveer0906@gmail.com>
2025-11-25 19:12:28 +0000

    Completed Exercise 3

---
+++ w1d0/w1do_answers.py
@@ -0,0 +1,7 @@
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+from aisb_utils import report
+import requests
+from typing import Callable
+print("It works!") 
